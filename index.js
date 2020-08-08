9bb7868dd677425f9d8d9c26d970b91241f44de2
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 15:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i=0; i<musicians.length; i++) {
+    array[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return array
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while(i <= facts.length) {
+    facts[i] = facts[i] + "!!!";
+    i++;
+    console.log(i)
+    console.log(facts.length)
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var store = []
+  do {
+    store.push("I love the Beatles!")
+  }
+  while(number<15);
+  
+  return store
+}
+
